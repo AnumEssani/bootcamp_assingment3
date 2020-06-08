fd1bd8cb993ba2184d1d877fba079a33a64fb0d4
AnumEssani <essanianum@gmail.com>
2020-06-08 23:45:43 +0000

    update dinner js

--- src/dinner.js
+++ src/dinner.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import './App.css'
 
 
 function Dinner (props) {
   return (
     <div > 
       <h1> {props.dishname} is so delicious </h1>
+      <hr/>
       <h1>I love {props.sweetdish} </h1>
       <hr/>
-      <h1>but my all time favorite is {props.fruit} &hearts; &hearts; &hearts; </h1>
+      <h1>but my all time favorite is {props.fruit} <span> &hearts; &hearts; &hearts;</span> </h1>
 
     </div>
   )
